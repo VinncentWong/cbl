2181913e8285caf99c987a1bfee3755729508fa6
VinncentWong <vinncentwong2@gmail.com>
2025-05-25 17:27:19 +0700

    utilize edited numeric

--- 001-balance-ops.cbl
+++ 001-balance-ops.cbl
@@ -13,6 +13,8 @@
        WORKING-STORAGE SECTION.
        77  BALANCE PIC S9(10)V99 VALUE ZEROES.
        77  OPERATED-BALANCE PIC S9(10)V99.
+       77  DISPLAY-BALANCE PIC Z,ZZZ,ZZZ.99.
+
        77  OPERATION PIC X(10).
        77  CONTINUE-OPERATION PIC X.
        PROCEDURE DIVISION.
@@ -39,11 +41,14 @@
            DISPLAY 'How many balance you want to add?: '.
            ACCEPT OPERATED-BALANCE.
            COMPUTE BALANCE = BALANCE + OPERATED-BALANCE.
-           DISPLAY 'Success add your balance. Current Balance: ' BALANCE
-           .
+           MOVE BALANCE TO DISPLAY-BALANCE.
+           DISPLAY 'Success add your balance. Current Balance: '
+               DISPLAY-BALANCE.
 
        SUBTRACT-BALANCE.
            DISPLAY 'How many balance you want to subtract?: '.
            ACCEPT OPERATED-BALANCE.
            COMPUTE BALANCE = BALANCE - OPERATED-BALANCE
-           .
+           MOVE BALANCE TO DISPLAY-BALANCE.
+           DISPLAY 'Success subtract your balance. Current Balance: '
+               DISPLAY-BALANCE.
